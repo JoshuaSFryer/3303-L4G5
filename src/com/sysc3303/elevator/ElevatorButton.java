716eb46e24dd404c49ae643626f0b1ccd5053db8
Josh <josh.s.fryer@gmail.com>
2019-02-01 17:44:35 -0500

    Implement ElevatorButton without message to scheduler

--- src/com/sysc3303/elevator/ElevatorButton.java
+++ src/com/sysc3303/elevator/ElevatorButton.java
@@ -1,5 +1,21 @@
 package com.sysc3303.elevator;
 
 public class ElevatorButton {
-
+	
+	private int id;
+	private Elevator parent;
+	private boolean illuminated;
+	
+	public ElevatorButton(Elevator parent, int id) {
+		this.id = id;
+	}
+	
+	public void ClickButton() {
+		// Have the elevator call method to send to scheduler, and turn on light.
+		setLight(true);
+	}
+	
+	private void setLight(boolean state) {
+		this.illuminated = state;
+	}
 }
