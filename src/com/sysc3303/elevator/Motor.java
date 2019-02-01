3c164e6bb4abd725ad6b722d9b13e965c306a64c
Josh <josh.s.fryer@gmail.com>
2019-02-01 17:46:09 -0500

    Implement Motor movement

--- src/com/sysc3303/elevator/Motor.java
+++ src/com/sysc3303/elevator/Motor.java
@@ -1,17 +1,20 @@
 package com.sysc3303.elevator;
 
 public class Motor {
-
-	public Motor() {
-		
+	
+	private final double INCREMENT = 0.1;
+	private Elevator parent;
+	
+	public Motor(Elevator parent) {
+		this.parent = parent;
 	}
 	
 	public void moveUp() {
-		
+		parent.setCurrentHeight(parent.getCurrentHeight() + INCREMENT);
 	}
 	
 	public void moveDown() {
-		
+		parent.setCurrentHeight(parent.getCurrentHeight() - INCREMENT);
 	}
 	
 	public void stop() {
