1bec6c1bf0e807212bee724c7fa1d94ee764c02c
Josh <josh.s.fryer@gmail.com>
2019-02-01 03:04:45 -0500

    Create FloorSensor class

---
+++ src/com/sysc3303/elevator/FloorSensor.java
@@ -0,0 +1,12 @@
+package com.sysc3303.elevator;
+
+public class FloorSensor {
+
+	public FloorSensor() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getFloor() {
+		return 0;
+	}
+}
