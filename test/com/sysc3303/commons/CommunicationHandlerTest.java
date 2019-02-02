7834254e205465719a032bfcaf698d23823bb211
matt1light <matt1light@gmail.com>
2019-02-02 09:29:02 -0500

    Add test for communication Handler

--- test/com/sysc3303/commons/CommunicationHandlerTest.java
+++ test/com/sysc3303/commons/CommunicationHandlerTest.java
@@ -5,24 +5,33 @@ import org.junit.Test;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Date;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.fail;
 
 public class CommunicationHandlerTest{
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException{
         // Two message Handlers
         TestMessageHandler messageHandler1 = new TestMessageHandler();
         TestMessageHandler messageHandler2 = new TestMessageHandler();
         // Two Communication Handlers
-        CommunicationHandler communicationHandler1 = new CommunicationHandler(3000, messageHandler1);
-        CommunicationHandler communicationHandler2 = new CommunicationHandler(5000, messageHandler2);
+        CommunicationHandler communicationHandler1 = new CommunicationHandler(7000, messageHandler1);
+        CommunicationHandler communicationHandler2 = new CommunicationHandler(7001, messageHandler2);
         // Make a message and send it from one to the other
-        FloorArrivalMessage message = new FloorArrivalMessage(1, Direction.DOWN);
+        FloorArrivalMessage message1 = new FloorArrivalMessage(1, Direction.DOWN);
+        FloorButtonMessage message2 = new FloorButtonMessage(1, Direction.DOWN, new Date());
         try {
-            communicationHandler1.send(message, InetAddress.getLocalHost(), 5000);
+            communicationHandler1.send(message1, InetAddress.getLocalHost(), 7001);
         } catch (UnknownHostException e){
             fail();
         }
+        try {
+            communicationHandler2.send(message2, InetAddress.getLocalHost(), 7000);
+        } catch (UnknownHostException e){
+            fail();
+        }
+        sleep(30000);
     }
 }
--- test/com/sysc3303/commons/TestMessageHandler.java
+++ test/com/sysc3303/commons/TestMessageHandler.java
@@ -3,7 +3,7 @@ package com.sysc3303.commons;
 public class TestMessageHandler implements IMessageHandler{
     @Override
     public void received(Message message) {
-        System.out.println("received message" + message);
+        System.out.println("Received message\n" + message);
     }
 
     public TestMessageHandler(){
