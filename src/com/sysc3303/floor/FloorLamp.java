ede3ce1685ded6aa77ba48c2c1374c312f28a743
tanvir1990 <pavel.mhp@gmail.com>
2019-02-02 10:16:23 -0500

    Worked-logic for Lamps, working-test cases

--- src/com/sysc3303/floor/FloorLamp.java
+++ src/com/sysc3303/floor/FloorLamp.java
@@ -1,30 +1,73 @@
 package com.sysc3303.floor;
 
 public class FloorLamp {
-	private boolean up   = false;
-	private boolean down = false;
 	
-	public void lightUp() {
-		up = true;
+	private String up;
+	private String down;
+	private String elevatorArrived;
+	
+	/*
+	 * The Simulator System will send integers for the direction requested by the passenger.
+	 * The floor Class should compare those integers with Strings "up", "down" or "elevatorArrived".
+	 *
+	 * Method flashLamp prints out statement based on which direction-button was pressed by the 
+	 * passenger on the floor. This information is sent from the Input Simulator. 
+	 * 
+	 * @param direction, direction requested by the passenger
+	 */
+	public void flashLamp(String direction) {
+		if (direction.equals(up)) {
+			//UP LAMP is ON, lampUP = ON
+			System.out.println("Direction Light UP is ON\n");
+		}
+		
+		else if (direction.equals(down)) {
+			//DOWN LAMP is ON, lampDOWN = ON
+			System.out.println("Direction Light DOWN is ON\n");
+		}
+		
+		//If the Elevator has Arrived on the destination floor, then both lights should be turned OFF
+		else if (direction.equals(elevatorArrived)) {
+			// Keep both the Lamps turned off All the time
+			System.out.println("Direction Lights are OFF\n");
+		}
+		
 	}
 	
-	public void lightDown() {
-		down = true;
+	/*
+	 * We also have another lamp that says if the Elevator has Arrived.
+	 * 
+	 * Method flashArrivalLamp turns ON if the Elevator has Arrived. Otherwise, it stays OFF
+	 */
+	
+	public void flashArrivalLamp(String arrivalmessage) {
+		if (arrivalmessage.equals(elevatorArrived)) {
+			System.out.println("ElevatorArrivalLamp turned ON, ElevatorHas Arrived\n");
+		}
+		
+		else {
+			System.out.println("Elevator Arrival is OFF\n");
+		}
 	}
 	
-	public void disableLightUp() {
-		up = false;
-	}
-	
-	public void disableLightDown() {
-		down = false;
-	}
-	
-	public boolean isUpLightEnabled() {
-		return up;
-	}
-	
-	public boolean isDownLightEnabled() {
-		return down;
-	}
+//	public void lightDown(boolean ) {
+//		down = true;
+//		System.out.println("Direction Light DOWN is ON");
+//	}
+//	
+//	public void disableLightUp() {
+//		up = false;
+//	}
+//	
+//	public void disableLightDown() {
+//		down = false;
+//	}
+//	
+//	public boolean isUpLightEnabled() {
+//		return up;
+//	}
+//	
+//	public boolean isDownLightEnabled() {
+//		return down;
+//	}
 }
