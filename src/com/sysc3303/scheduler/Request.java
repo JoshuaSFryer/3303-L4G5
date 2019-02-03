db8f2b6272728355d24f081c07f1f35da2169173
xinuy77 <yuyamanaka@cmail.carleton.ca>
2019-02-02 20:43:47 -0500

    commenting

--- src/com/sysc3303/scheduler/Request.java
+++ src/com/sysc3303/scheduler/Request.java
@@ -6,6 +6,10 @@ import com.sysc3303.commons.ElevatorButtonMessage;
 import com.sysc3303.commons.FloorButtonMessage;
 import com.sysc3303.elevator.ElevatorVector;
 
+/**
+ * @author Yu Yamanaka Xinrui Zhang
+ *
+ */
 public class Request {
 		private ArrayList<FloorButtonMessage>    floorRequestList;
 		private ArrayList<ElevatorButtonMessage> elevatorRequestList;
@@ -47,26 +51,46 @@ public class Request {
 			return floorRequestList;
 		}
 		
+		/**
+		 * @return ArrayList<ElevatorButtonMessage>
+		 */
 		public synchronized ArrayList<ElevatorButtonMessage> getElevatorButtonMessageArray() {
 			return elevatorRequestList;
 		}
 		
+		/**
+		 * @param index
+		 * @return ElevatorButtonMessage
+		 */
 		public synchronized ElevatorButtonMessage getElevatorButtonMessage(int index) {
 			return elevatorRequestList.get(index);
 		}
 		
+		/**
+		 * 
+		 * @param floorButtonMessage
+		 */
 		public synchronized void addFloorButtonMessage(FloorButtonMessage floorButtonMessage) {
 			floorRequestList.add(floorButtonMessage);
 		}
 		
+		/**
+		 * @param elevatorButtonMessage
+		 */
 		public synchronized void addElevatorButtonMessage(ElevatorButtonMessage elevatorButtonMessage) {
 			elevatorRequestList.add(elevatorButtonMessage);
 		}
 		
+		/**
+		 * @param floorButtonMessageArr
+		 */
 		public synchronized void setFloorButtonMessageArray(ArrayList<FloorButtonMessage> floorButtonMessageArr) {
 			floorRequestList = floorButtonMessageArr;
 		}
 		
+		/**
+		 * @param elevatorButtonMessageArr
+		 */
 		public synchronized void setElevatorButtonMessageArray(ArrayList<ElevatorButtonMessage> elevatorButtonMessageArr) {
 			elevatorRequestList = elevatorButtonMessageArr;
 		}	
