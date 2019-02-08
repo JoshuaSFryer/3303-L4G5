fa1479c01890840cb8cae0803f1febb4358ec554
matt1light <matt1light@gmail.com>
2019-02-07 23:28:32 -0500

    modify config path to reflect new project structure

--- src/main/java/com/sysc3303/constants/Constants.java
+++ src/main/java/com/sysc3303/constants/Constants.java
@@ -2,6 +2,6 @@ package com.sysc3303.constants;
 
 public class Constants {
 	private Constants() {}
-	public static final String CONFIG_PATH = "./src/config/config.properties";
-	public static final String INPUT_PATH  = "./src/resources/inputFile.txt";
+	public static final String CONFIG_PATH = "./src/main/java/config/config.properties";
+	public static final String INPUT_PATH  = "./src/main/java/resources/inputFile.txt";
 }
