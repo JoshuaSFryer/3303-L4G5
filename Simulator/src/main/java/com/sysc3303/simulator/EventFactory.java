22539fc203fd9c76c735be1e8dc6613ea06f7f97
matt1light <matt1light@gmail.com>
2019-02-28 12:22:26 -0500

    Refactor Request event out of event, and make creation occur through a factory

---
+++ Simulator/src/main/java/com/sysc3303/simulator/EventFactory.java
@@ -0,0 +1,28 @@
+package com.sysc3303.simulator;
+
+import java.text.ParseException;
+
+public class EventFactory {
+
+    public Event makeEvent(String[] eventStringArray) throws ParseException {
+        Event event = null;
+        // the type of event is specified as the second string in the event file
+        switch(eventStringArray[1]){
+            case "request":
+                event = new RequestEvent(eventStringArray);
+                break;
+            case "stick_door":
+                // make door stick error event
+                event = new DoorStickEvent(eventStringArray);
+                break;
+            case "stick_elevator":
+                // make elevator stick
+                event = new ElevatorStickEvent(eventStringArray);
+                break;
+            case default:
+                // if neither, print type does not exist
+                System.out.println("Event type : " + eventStringArray[1] + " does not exist");
+                break;
+        }
+    }
+}
---
+++ Simulator/src/main/java/com/sysc3303/simulator/RequestEvent.java
@@ -0,0 +1,51 @@
+package com.sysc3303.simulator;
+import com.sysc3303.commons.Direction;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+public class RequestEvent extends Event{
+    private int floor, elevatorButton;
+    private Direction direction;
+
+    /**
+     * Constructor that takes the parameters as one array of strings that need to be parsed
+     * @param stringArray
+     * @throws ParseException
+     */
+    public RequestEvent(String[] stringArray) throws ParseException{
+        super(stringArray);
+        this.floor = Integer.parseInt(stringArray[1]);
+        this.direction = Direction.valueOf(stringArray[2].toUpperCase());
+        this.elevatorButton = Integer.parseInt(stringArray[3]);
+    }
+
+    public String toString(){
+        return "RequestEvent:\n\tTimestamp: " + timestamp + "\n\tFloor: " + floor + "\n\tDirection: " + direction + "\n\tElevator Button: " + elevatorButton;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getElevatorButton() {
+        return elevatorButton;
+    }
+
+    public void setElevatorButton(int elevatorButton) {
+        this.elevatorButton = elevatorButton;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
