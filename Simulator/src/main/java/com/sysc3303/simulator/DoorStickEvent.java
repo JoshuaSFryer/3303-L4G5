4c98023efe0f7eb86906bf7fa18f4198eec79d06
matt1light <matt1light@gmail.com>
2019-02-28 13:13:02 -0500

    testing error event sending in the simulator

--- Simulator/src/main/java/com/sysc3303/simulator/DoorStickEvent.java
+++ Simulator/src/main/java/com/sysc3303/simulator/DoorStickEvent.java
@@ -8,7 +8,13 @@ public class DoorStickEvent extends StickEvent{
         super(eventStringArray);
     }
 
+    // sends the door stick request to the elevator system
     public void send(){
         ElevatorSender.getInstance().sendDoorStick(elevatorId, numSecondsStuck);
+    }
+
+    @Override
+    public String toString(){
+        return "DoorStickEvent:\n\tTimestamp: " + timestamp + "\n\tElevatorId: " + elevatorId + "\n\tnumSecondsStuck: " + numSecondsStuck;
     }
 }
