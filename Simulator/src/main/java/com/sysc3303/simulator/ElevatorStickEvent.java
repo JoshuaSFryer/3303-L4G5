6256c6e266bc8c786ef345d6f357009add9092b6
matt1light <matt1light@gmail.com>
2019-02-28 12:28:38 -0500

    Add door stick and elevator stick events

---
+++ Simulator/src/main/java/com/sysc3303/simulator/ElevatorStickEvent.java
@@ -0,0 +1,11 @@
+package com.sysc3303.simulator;
+
+import java.text.ParseException;
+
+public class ElevatorStickEvent extends StickEvent{
+
+    public ElevatorStickEvent(String[] eventStringArray) throws ParseException {
+        super(eventStringArray);
+    }
+}
+
