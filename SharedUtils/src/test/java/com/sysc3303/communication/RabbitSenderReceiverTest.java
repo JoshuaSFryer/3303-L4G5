b6bec67a81913ba9cc4381218a5a4047e6fb5fb7
matt1light <matt1light@gmail.com>
2019-03-03 11:19:57 -0500

    add support for the nested json in ElevatorStateMessage
    Added tests to verify that it works
    Add JsonCreator tags to ElevatorVector

--- SharedUtils/src/test/java/com/sysc3303/communication/RabbitSenderReceiverTest.java
+++ SharedUtils/src/test/java/com/sysc3303/communication/RabbitSenderReceiverTest.java
@@ -1,24 +1,48 @@
 package com.sysc3303.communication;
 
 import com.sysc3303.commons.Direction;
+import com.sysc3303.commons.ElevatorVector;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static java.lang.Thread.sleep;
 
 public class RabbitSenderReceiverTest {
-    @Test
-    public void test1() throws InterruptedException{
-        // create a receiver
-        TestMessageHandler messageHandler = new TestMessageHandler(2000);
-        RabbitReceiver receiver = new RabbitReceiver(messageHandler, "test_queue");
-        // create a sender
-        RabbitSender sender = new RabbitSender("test_queue", new FloorArrivalMessage(1, Direction.UP, 1));
+    static TestMessageHandler messageHandler;
+    static RabbitReceiver receiver;
 
+    @BeforeClass
+    public static void setUpClass(){
+        messageHandler = new TestMessageHandler(2000);
+    }
+
+    @Test
+    public void testRegularMessage() throws InterruptedException{
+        RabbitReceiver receiver = new RabbitReceiver(messageHandler, "regular_test");
+        // create a sender
+        RabbitSender sender = new RabbitSender("regular_test", new FloorArrivalMessage(1, Direction.UP, 1));
+
+        (new Thread(receiver)).run();
+        // start the sender
+        (new Thread(sender)).run();
+
+        //sleep 2 seconds to allow time to receive
+        sleep(2000);
+    }
+    @Test
+    public void testNestedMessage() throws InterruptedException{
+        RabbitReceiver receiver = new RabbitReceiver(messageHandler, "nested_test");
+        // create a sender
+        RabbitSender sender = new RabbitSender("nested_test", new ElevatorStateMessage(new ElevatorVector(1, Direction.UP, 1), 1));
+
+        (new Thread(receiver)).run();
         // start the receiver
         (new Thread(receiver)).run();
         // start the sender
         (new Thread(sender)).run();
-        //sleep for some seconds
+        //sleep 2 seconds to allow time to receive
         sleep(2000);
     }
 }
