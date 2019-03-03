0848d38f548fcf6f412c54444d3eeebe1f1a9f88
matt1light <matt1light@gmail.com>
2019-03-02 20:42:10 -0500

    add RabbitSender and RabbitReceiver as well as a test to verify that they are functioning properly

--- SharedUtils/src/main/java/com/sysc3303/communication/RabbitReceiver.java
+++ SharedUtils/src/main/java/com/sysc3303/communication/RabbitReceiver.java
@@ -4,43 +4,50 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+import com.sysc3303.commons.SerializationUtil;
 
+public class RabbitReceiver implements Runnable {
 
-public class RabbitReceiver {
+    private final static boolean AUTO_ACK = true;
+    private final static String HOSTNAME = "localhost";
+    private MessageHandler messageHandler;
+    private SerializationUtil<Message> serializationUtil;
+    private String queueName;
 
-    private final static String QUEUE_NAME = "hello";
-    private final static boolean AUTO_ACK = false;
-
-    public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-
-            System.out.println(" [x] Received '" + message + "'");
-
-            try {
-                doWork(message);
-            }catch(InterruptedException e) {
-            }
-                finally
-            {
-                System.out.println(" [x] Done");
-            }
-        };
-        channel.basicConsume(QUEUE_NAME, AUTO_ACK, deliverCallback, consumerTag -> {});
+    public RabbitReceiver(MessageHandler messageHandler, String queueName){
+        this.messageHandler = messageHandler;
+        this.queueName = queueName;
+        serializationUtil = new SerializationUtil<>();
     }
 
-    private static void doWork(String task) throws  InterruptedException {
-        for (char ch: task.toCharArray()){
-            if (ch == '.') Thread.sleep(1000);
+    public void run(){
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOSTNAME);
+        try{
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(queueName, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                Message message = serializationUtil.deserialize(delivery.getBody(), delivery.getBody().length);
+
+                System.out.println(" [x] Received '" + message + "'");
+
+                try {
+                    receive(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                }
+            };
+            channel.basicConsume(queueName, AUTO_ACK, deliverCallback, consumerTag -> {});
+        } catch (Exception e){
+            e.printStackTrace();
         }
+    }
+
+    private void receive(Message message) {
+        messageHandler.received(message);
     }
 }
--- SharedUtils/src/main/java/com/sysc3303/communication/RabbitSender.java
+++ SharedUtils/src/main/java/com/sysc3303/communication/RabbitSender.java
@@ -3,23 +3,33 @@ package com.sysc3303.communication;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.sysc3303.commons.SerializationUtil;
 
-public class RabbitSender {
-    private final static String QUEUE_NAME = "hello";
+public class RabbitSender implements Runnable{
 
-    public static void main(String[] argv) throws Exception {
+    private final static String HOSTNAME = "localhost";
+    private SerializationUtil<Message> serializationUtil;
+    private String queueName;
+    private Message message;
 
+    public RabbitSender(String queueName, Message message){
+        this.queueName = queueName;
+        this.message = message;
+        serializationUtil = new SerializationUtil<>();
+    }
+    public void run() {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost(HOSTNAME);
 
         try(Connection connection = factory.newConnection();
             Channel channel = connection.createChannel()){
 
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(queueName, false, false, false, null);
 
-            String message = String.join(" ", argv);
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            byte[] data = serializationUtil.serialize(message);
+            channel.basicPublish("", queueName, null, data);
             System.out.println(" [x] Sent '" + message + "'");
+        } catch (Exception e){
         }
     }
 }
