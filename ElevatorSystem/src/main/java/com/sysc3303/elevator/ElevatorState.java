8152c5cb7f7d3d42e46d26a77a30093096850aed
JoshuaSFryer <josh.s.fryer@gmail.com>
2019-03-08 16:31:41 -0500

    _

--- ElevatorSystem/src/main/java/com/sysc3303/elevator/ElevatorState.java
+++ ElevatorSystem/src/main/java/com/sysc3303/elevator/ElevatorState.java
@@ -1,151 +1,41 @@
 package com.sysc3303.elevator;
-/*
-import java.util.EnumSet;
-import java.util.Set;
 
-public enum ElevatorState {
-	IDLE {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(MOVINGUP, MOVINGDOWN, OPENINGDOORS);
-		}
-	},
-	
-	MOVINGUP {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(IDLE);
-		}
-	},
-	
-	MOVINGDOWN {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(IDLE);
-		}
-	},
-	
-	OPENINGDOORS {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(DOORSOPEN);
-		}
-	},
-	
-	DOORSOPEN {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(DOORSCLOSING);
-		}
-	},
-	
-	DOORSCLOSING {
-		@Override
-		public Set<ElevatorState> nextStates() {
-			return EnumSet.of(IDLE);
-		}
-	};
-	
-	public Set<ElevatorState> nextStates() {
-		return EnumSet.noneOf(ElevatorState.class);
-	}
-}
-*/
+public abstract class ElevatorState {
+    private Elevator parent;
 
-abstract interface ElevatorState {
-	public void entryAction(Elevator context);
-	public void doAction(Elevator context);
-	public void exitAction(Elevator context);
+    public ElevatorState(Elevator parent) {
+        this.parent = parent;
+    }
+
+    public void entryAction() {
+
+    }
+
+    public void exitAction() {
+
+    }
+
+    public void stateTransition() {
+
+    }
 }
 
-class Idle implements ElevatorState {
-	public void entryAction(Elevator context) {
-		
-	}
-	
-	public void doAction(Elevator context) {
-		
-	}
-	
-	public void exitAction(Elevator context) {
-		
-	}
+class Idle extends ElevatorState {
+
+    public Idle(Elevator parent) {
+        super(parent);
+    }
+
 }
 
-class MovingUp implements ElevatorState {
-	public void entryAction(Elevator context) {
-		//context.moveUp();
-	}
-	
-	public void doAction(Elevator context) {
-		
-	}
-	
-	public void exitAction(Elevator context) {
-		//context.stop();
-	}
-}
+class MovingUp extends ElevatorState {
 
-class MovingDown implements ElevatorState {
-	public void entryAction(Elevator context) {
-		//context.moveDown();
-	}
-	
-	public void doAction(Elevator context) {
-		
-	}
-	
-	public void exitAction(Elevator context) {
-		//context.stop();
-	}
-}
+    public MovingUp(Elevator parent) {
+        super(parent);
+    }
 
-class OpeningDoors implements ElevatorState {
-	public void entryAction(Elevator context) {
-		context.openDoors();
-	}
-	
-	public void doAction(Elevator context) {
-		
-	}
-	
-	public void exitAction(Elevator context) {
-		
-	}
-}
-
-class DoorsOpen implements ElevatorState {
-	public void entryAction(Elevator context) {
-		
-	}
-	
-	public void doAction(Elevator context) {
-		try {
-			Thread.sleep(20000);
-		} catch(InterruptedException e) {
-			// TODO: interrupt handling? or just let this pass through and break
-			// early?
-		}
-		// After time elapses, force the state to change.
-		context.setState(new ClosingDoors());
-	}
-	
-	public void exitAction(Elevator context) {
-		
-	}
-}
-
-class ClosingDoors implements ElevatorState {
-	public void entryAction(Elevator context) {
-		context.closeDoors();
-		context.setState(new Idle());
-	}
-	
-	public void doAction(Elevator context) {
-		
-	}
-	
-	public void exitAction(Elevator context) {
-		
-	}
+    @Override
+    public void entryAction() {
+        super.entryAction();
+    }
 }
