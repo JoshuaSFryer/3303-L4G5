2ce900dd52dbafc7ac9f11d0584eaf2c490f2b6e
tanvir1990 <pavel.mhp@gmail.com>
2019-03-20 17:57:24 -0400

    new files added for GUIFX

---
+++ GUIFX/src/main/java/com/sysc3303/Controller.java
@@ -0,0 +1,9 @@
+package com.sysc3303;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressButton(ActionEvent event){
+        System.out.println("Hello, Tanvir");
+    }
+}
