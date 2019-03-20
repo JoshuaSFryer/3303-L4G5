5cd06898ea1bc6f1eb6a75b1c16b62e1ea6e3bbc
matt1light <matt1light@gmail.com>
2019-03-20 08:04:56 -0400

    Google cloud integration for rabbitmq

--- SharedUtils/src/main/java/com/sysc3303/communication/RabbitSender.java
+++ SharedUtils/src/main/java/com/sysc3303/communication/RabbitSender.java
@@ -21,7 +21,10 @@ public class RabbitSender implements Runnable{
     public void run() {
         ConnectionFactory factory = new ConnectionFactory();
         String hostname;
-        if (Boolean.parseBoolean(ConfigProperties.getInstance().getProperty("local"))){
+        if (Boolean.parseBoolean(ConfigProperties.getInstance().getProperty("rabbitCloud"))){
+          hostname = ConfigProperties.getInstance().getProperty("rabbitCloudAddress");
+        }
+        else if (Boolean.parseBoolean(ConfigProperties.getInstance().getProperty("local"))){
             hostname = "localhost";
         }
         else {
