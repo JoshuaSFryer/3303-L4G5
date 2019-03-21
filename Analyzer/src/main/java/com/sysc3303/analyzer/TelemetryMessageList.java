a36532a4a4de12475480e2c567e9fd86bde4f553
matt1light <matt1light@gmail.com>
2019-03-20 22:43:29 -0400

    add math for analyzing response data calculating avg and variance

--- Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMessageList.java
+++ Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMessageList.java
@@ -7,26 +7,41 @@ public class TelemetryMessageList {
     public List<Long> elevatorButtonTimeList;
     public List<Long> floorButtonTimeList;
     public List<Long> arrivalTimeList;
+    public TelemetryMath math;
 
     public TelemetryMessageList(){
         arrivalTimeList = new ArrayList<>();
         floorButtonTimeList = new ArrayList<>();
         elevatorButtonTimeList = new ArrayList<>();
+        math = TelemetryMath.getInstance();
     }
 
     public void addArrivalTime(long time){
         System.out.println(time + " added to arrival time list");
         arrivalTimeList.add(time);
+        long avg = math.getMean(arrivalTimeList);
+        long var = math.getVariance(arrivalTimeList, avg);
+        printAnalysis("Arrival response", arrivalTimeList);
     }
 
     public void addFloorButtonTime(long time){
         System.out.println(time + " added to floor button list");
         floorButtonTimeList.add(time);
+        printAnalysis("Floor Button Response", floorButtonTimeList);
     }
 
     public void addElevatorButtonTime(long time){
         System.out.println(time + " added to elevator button list");
         elevatorButtonTimeList.add(time);
+        printAnalysis("Elevator Button Response", elevatorButtonTimeList);
+    }
+
+    private void printAnalysis(String name, List<Long> list){
+        long avg = math.getMean(list);
+        long var = math.getVariance(list, avg);
+        System.out.println(name + ":");
+        System.out.println("\tMean: " + avg + "ns");
+        System.out.println("\tVariance: " + var + "ns");
     }
 
 }
