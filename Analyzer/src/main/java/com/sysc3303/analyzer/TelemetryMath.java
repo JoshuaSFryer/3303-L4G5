c9ddf74770af9ab8ccd9e4124f45dc0b8686d5ed
matt1light <matt1light@gmail.com>
2019-03-22 09:40:49 -0400

    Add comments to analyzer

--- Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMath.java
+++ Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMath.java
@@ -2,13 +2,25 @@ package com.sysc3303.analyzer;
 
 import java.util.List;
 
+/**
+ * Used to do mathematical operations on lists of longs
+ * Can calculate mean and variance
+ */
 public class TelemetryMath {
 
-
+    /**
+     * Constructor replacement
+     * @return a new TelemetryMath instance
+     */
     public static TelemetryMath getInstance(){
         return new TelemetryMath();
     }
 
+    /**
+     * calculates the mean of the elements in the list
+     * @param list the list with the elements in it
+     * @return the mean of the elements in the list
+     */
     public long getMean(List<Long> list){
         long sum = 0;
         for (Long num: list){
@@ -17,11 +29,23 @@ public class TelemetryMath {
         return sum;
     }
 
+    /**
+     * calculates the variance of the elements in the list
+     * @param list the list with the elements in it
+     * @return the variance of the elements in the list
+     */
     public long getVariance(List<Long> list){
         long mean = getMean(list);
         return getVariance(list, mean);
     }
 
+    /**
+     * calculates the variance of the elements in the list given the mean of the list
+     * Saves one iteration of the list if the mean has already been calculated
+     * @param list the list with the elements in it
+     * @param mean the mean of the elements in the list
+     * @return the variance of the elements in the list
+     */
     public long getVariance(List<Long> list, long mean){
         long var = 0;
         long size = list.size();
@@ -32,6 +56,9 @@ public class TelemetryMath {
     }
 
 
+    /**
+     * Private constructor prevents newing of the object
+     */
     private TelemetryMath(){
     }
 }
