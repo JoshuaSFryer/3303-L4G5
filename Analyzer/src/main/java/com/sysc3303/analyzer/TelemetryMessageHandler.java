939f724b41f420fa143e9fbb0c765e1e626a5945
matt1light <matt1light@gmail.com>
2019-03-22 09:40:49 -0400

    Add comments to analyzer

--- Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMessageHandler.java
+++ Analyzer/src/main/java/com/sysc3303/analyzer/TelemetryMessageHandler.java
@@ -4,11 +4,20 @@ import com.sysc3303.communication.Message;
 import com.sysc3303.communication.MessageHandler;
 import com.sysc3303.communication.TelemetryMessage;
 
+/**
+ * Message handler for handling the reception of Telemetry data
+ *
+ */
+
 public class TelemetryMessageHandler extends MessageHandler {
     private static TelemetryMessageHandler instance;
     private TelemetryMessageList messageList;
     private TelemetryMath math;
 
+    /**
+     * If there is an existing instance return that, if not make a new one and return it
+     * @return an instance of TelemetryMessageHandler
+     */
     public static TelemetryMessageHandler getInstance(){
         if (instance == null){
             instance = new TelemetryMessageHandler();
@@ -16,12 +25,19 @@ public class TelemetryMessageHandler extends MessageHandler {
         return instance;
     }
 
+    /**
+     * Private constructor prevents use outside of the class and ensures only one instance exists at a time
+     */
     private TelemetryMessageHandler(){
         super();
         messageList = new TelemetryMessageList();
     }
 
 
+    /**
+     * Performs the appropriate action to process a message that is received
+     * @param message the message to be processed
+     */
     public void received(Message message){
         super.received(message);
         switch(message.getOpcode()){
@@ -38,3 +54,4 @@ public class TelemetryMessageHandler extends MessageHandler {
     }
 
 }
+
