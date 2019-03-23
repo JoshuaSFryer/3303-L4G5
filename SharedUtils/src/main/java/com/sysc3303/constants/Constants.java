adfff8ba5bd66c7e20015cc0f9361e3f45d24a60
xinuy77 <yuyamanaka@cmail.carleton.ca>
2019-03-23 10:46:20 -0400

    change measurement from System.nanoTime to System.currentTimeMillis

--- SharedUtils/src/main/java/com/sysc3303/constants/Constants.java
+++ SharedUtils/src/main/java/com/sysc3303/constants/Constants.java
@@ -2,7 +2,9 @@ package com.sysc3303.constants;
 
 public class Constants {
 	private Constants() {}
-	public static final String CONFIG_PATH = "/config.properties";
-	public static final String INPUT_PATH  = "./resources/inputFile.txt";
+	public static final String CONFIG_PATH    = "/config.properties";
+	public static final String INPUT_PATH     = "./resources/inputFile.txt";
+	public static final int    NANO_PER_MILLI = 1000000;
+    public static final int    NANO_PER_SEC   = 1000000000;
 }
 
