3e147f7e8dffadda26e9998ad5383dd3d86a1a6d
tanvir1990 <pavel.mhp@gmail.com>
2019-03-27 17:50:36 -0400

    Floor Buttons Distinguished

---
+++ GUIFX/src/main/java/com/sysc3303/CustomButton.java
@@ -0,0 +1,60 @@
+package com.sysc3303;
+
+import com.sysc3303.commons.ConfigProperties;
+import com.sysc3303.commons.Direction;
+import com.sysc3303.communication.FloorClickSimulationMessage;
+import com.sysc3303.communication.RabbitSender;
+import javafx.scene.control.Button;
+
+import static com.sysc3303.commons.Direction.UP;
+import static com.sysc3303.commons.Direction.DOWN;
+public class CustomButton extends Button {
+    int floorNumber;
+    Direction dir;
+    static String floorQueueName = ConfigProperties.getInstance().getProperty("floorQueueName");
+
+     CustomButton(int floorNumber, Direction dir, String str){
+
+         super(str);
+
+        this.floorNumber = floorNumber;
+        this.dir = dir;
+
+        this.setOnAction((event) -> {
+            sendFloorClick(floorNumber, this.dir);
+            System.out.println("BUtton pressed!");
+        });
+
+    }
+    //This method creates the 
+    public static CustomButton create(int floorNumber, Direction dir){
+
+        if (dir==UP){
+            return new CustomButton(floorNumber, dir, "UP");
+
+        }
+        else if (dir == DOWN){
+            return new CustomButton(floorNumber, dir, "DOWN");
+        }
+
+        else { // invalid case
+            System.out.println("Invalid direction provided when creating button");
+            return null;
+        }
+
+    }
+    // This method sends messages to RabbitMQ
+    private void sendFloorClick(int floorNumber, Direction direction){
+         FloorClickSimulationMessage message = new FloorClickSimulationMessage(floorNumber, direction);
+         RabbitSender sender = new RabbitSender(floorQueueName, message);
+         new Thread(sender).start();
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+
+    public void setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+}
