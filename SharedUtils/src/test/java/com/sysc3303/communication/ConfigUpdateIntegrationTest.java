ce9d7af65fb50c64a17ac04d6cdea2b967f920d8
matt1light <matt1light@gmail.com>
2019-03-27 20:39:11 -0400

    Test config update across system

---
+++ SharedUtils/src/test/java/com/sysc3303/communication/ConfigUpdateIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.sysc3303.communication;
+
+import com.sysc3303.commons.ConfigUpdater;
+import org.junit.Test;
+
+import static java.lang.Thread.sleep;
+
+public class ConfigUpdateIntegrationTest {
+    @Test
+    /**
+     * Make sure to run the whole system before running this integration test
+     */
+    public void script(){
+        ConfigUpdater updater = ConfigUpdater.getInstance();
+        String[][] configUpdates = {
+                {
+                        "numberOfFloors",
+                        "10"
+                },
+                {
+                        "numberOfElevators",
+                        "2"
+                }
+        };
+        updater.updateConfigs(configUpdates);
+        try{
+            sleep(2000);
+        }catch(InterruptedException e){
+        }
+    }
+}
