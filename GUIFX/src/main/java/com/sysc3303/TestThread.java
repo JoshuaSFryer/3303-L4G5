58d38c6a3bcf29874d5d4467d193f1ef8b51f750
tanvir1990 <pavel.mhp@gmail.com>
2019-03-29 13:28:27 -0400

    Add ability to move elevators

---
+++ GUIFX/src/main/java/com/sysc3303/TestThread.java
@@ -0,0 +1,16 @@
+//package com.sysc3303;
+//
+//public class TestThread implements Runnable {
+//
+//    Main context;
+//
+//    public TestThread(Main m) {
+//        this.context = m;
+//    }
+//
+//    public void run() {
+//        for (int i=0; i<5; i++) {
+//            context.moveElevator(2, i);
+//        }
+//    }
+//}
