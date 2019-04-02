ae90ca429cb7ad49a9c74debb27cb27a63a08bac
tanvir1990 <pavel.mhp@gmail.com>
2019-04-02 14:34:31 -0400

    Doors are opening, rectangle changes color, and pop up window is working

--- GUIFX/src/main/java/com/sysc3303/CustomButton.java
+++ GUIFX/src/main/java/com/sysc3303/CustomButton.java
@@ -21,8 +21,10 @@ public class CustomButton extends Button  {
         this.dir = dir;
 
         this.setOnAction((event) -> {
+            this.getStyleClass().removeAll("button");
             sendFloorClick(floorNumber, this.dir);
-            System.out.println("BUtton pressed!");
+            System.out.println("Floor Button pressed!");
+            this.getStyleClass().add("buttonChanged");
         });
 
     }
@@ -56,6 +58,18 @@ public class CustomButton extends Button  {
 
     public void setFloorNumber(int floorNumber) {
         this.floorNumber = floorNumber;
+    }
+
+    public void unClickButton(){
+         this.getStyleClass().removeAll("buttonChanged");
+        System.out.println("Button Unclicked");
+         this.getStyleClass().add("button");
+    }
+
+    public void clickButton() {
+        this.getStyleClass().removeAll("button");
+        this.getStyleClass().add("buttonChanged");
+        System.out.println("Button Clicked");
     }
 
 //    @Override
