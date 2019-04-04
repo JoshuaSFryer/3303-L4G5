4c7fe7e65bc67fde69b4839831f66d5736b4fc06
Xinrui Zhang <serena2766@gmail.com>
2019-04-04 14:13:59 -0400

    add javadoc

--- Scheduler/src/main/java/com/sysc3303/scheduler/ElevatorStateMessageValidator.java
+++ Scheduler/src/main/java/com/sysc3303/scheduler/ElevatorStateMessageValidator.java
@@ -4,7 +4,18 @@ import com.sysc3303.commons.Direction;
 import com.sysc3303.commons.ElevatorVector;
 import com.sysc3303.communication.ElevatorStateMessage;
 
+/**
+ * Elevator state message validator
+ * @author Yu Yamanaka Xinrui Zhang
+ */
 public class ElevatorStateMessageValidator {
+	/**
+	 * The function checks if the elevator state message is a valid message
+	 * return true if it is, false otherwise
+	 * @param message
+	 * @param request
+	 * @return boolean
+	 */
 	public boolean isValidStateMessage(ElevatorStateMessage message, Request request) {
 		int            elevatorId     = message.getElevatorId();
 		ElevatorVector elevatorVector = request.getElevatorVector(elevatorId);
