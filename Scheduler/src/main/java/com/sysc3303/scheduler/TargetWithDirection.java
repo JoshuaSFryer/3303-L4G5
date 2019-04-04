2d2420aed4a158e9b1fa4e7a91b74f7a58627263
Xinrui Zhang <serena2766@gmail.com>
2019-04-04 13:39:51 -0400

    added javadoc comments

--- Scheduler/src/main/java/com/sysc3303/scheduler/TargetWithDirection.java
+++ Scheduler/src/main/java/com/sysc3303/scheduler/TargetWithDirection.java
@@ -1,24 +1,45 @@
 package com.sysc3303.scheduler;
 
 import com.sysc3303.commons.Direction;
-
+/**
+ * This class stores target with direction.
+ * @author Xinrui Zhang Yu Yamanaka
+ *
+ */
 public class TargetWithDirection {
 	private final int       targetFloor;
 	private final Direction targetDirection;
 	
+	/**
+    	* The TargetWithDirection class constructer
+    	* @para targetFloor
+    	* @para targetDirection
+     	*/
 	public TargetWithDirection(int targetFloor, Direction targetDirection) {
 		this.targetFloor = targetFloor;
 		this.targetDirection = targetDirection;
 	}
-
+	
+	/**
+    	* Get the current target floor
+	* @return int
+     	*/
 	public int getTargetFloor() {
 		return targetFloor;
 	}
-
+	
+	/**
+    	* Get the direction of current target floor
+	* @return ditection
+     	*/
 	public Direction getTargetDirection() {
 		return targetDirection;
 	}
 	
+	/**
+    	* Get the string of the target floor with direction 
+	* @return String
+     	*/
 	public String toString() {
 		return "\n TargetFloor: " + targetFloor + 
 			   "\n TargetDirection: " + targetDirection;
